2312b2bf270a213ebb77b419d984cdcd68d007bf
anita-that-chiq <62019551+kahenya-anita@users.noreply.github.com>
2024-04-30 10:04:05 +0300

    React router

---
+++ src/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+
+function About() {
+  return (
+    <>
+      <header>
+        <NavBar />
+      </header>
+      <main>
+        <h1>This is the about component!</h1>
+      </main>
+    </>
+  );
+}
+
+export default About;
